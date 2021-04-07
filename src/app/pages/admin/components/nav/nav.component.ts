678241e80e270bc3784596ef36f222ee6f0c722a
Aaron Chacon <aaronchacon86@gmail.com>
2021-04-06 23:17:48 -0400

    new form product

--- src/app/pages/admin/components/nav/nav.component.ts
+++ src/app/pages/admin/components/nav/nav.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { Routes, Params, ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
+export class NavComponent implements DoCheck{
+
+  btnNP = true;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -16,6 +19,23 @@ export class NavComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router,
+    ) {
+
+  }
+
+  ngDoCheck(): void {
+    this.btnHidden()
+  }
+
+  btnHidden() {
+    if (this.router.url  == '/admin/create') {
+      this.btnNP = false;
+    } else {
+      this.btnNP = true;
+    }
+  }
 
 }
